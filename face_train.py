32e240ba23340e3a5196e121ca2658a0d3c8f0e9
Vikas Thapliyal <alexrocko90@gmail.com>
2019-05-06 22:07:32 +0530

    more changes

--- face_train.py
+++ face_train.py
@@ -1,4 +1,6 @@
 import os
+from pillow import Image
+import numpy as np
 
 BASE_DIR = os.path.dirnname(os.path.abspath(__file__))
 image_dir = os.path.join(BASE_DIR, "images")
@@ -7,3 +9,8 @@ for files, root, dirs in os.walk(image_dir):
 	for file in files:
 		if file.endswith("png") or file.endswith("jpg"):
 			path = path.os.join(root, file)
+			label = os.path.basename(root).replace(' ', '-').lower()
+			print(label, path)
+			pil_image = Image.open(path).convert('L') # L is for grayscale
+			image_array = np.array(pil_image, "uint8")
+			
